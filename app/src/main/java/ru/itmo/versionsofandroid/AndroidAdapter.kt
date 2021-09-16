9d37d32acc454f002ce5b93667af3c2e5d44828c
Irina Keil <35578733+Luftschutzkeller@users.noreply.github.com>
2021-09-17 00:27:36 +0300

    alarm: callback and getAdapterPosition added but there ar some bugz

--- app/src/main/java/ru/itmo/versionsofandroid/AndroidAdapter.kt
+++ app/src/main/java/ru/itmo/versionsofandroid/AndroidAdapter.kt
@@ -7,16 +7,17 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.*
 import ru.itmo.versionsofandroid.data.Android
 
 
-class AndroidAdapter(Context: Context, private val android: List<Android>,
-) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class AndroidAdapter(Context: Context, private val android: List<Android>, private val clickListener: (position: Int) -> Unit
+) : Adapter<AndroidAdapter.ViewHolder> () {
 
     private val inflater: LayoutInflater = LayoutInflater.from(Context)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return ViewHolder(inflater.inflate(R.layout.item_android, parent, false))
+        return ViewHolder(inflater.inflate(R.layout.item_android, parent, false), clickListener)
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
@@ -25,19 +26,24 @@ class AndroidAdapter(Context: Context, private val android: List<Android>,
 
     override fun getItemCount(): Int = android.size
 
-    private fun getItem(position: Int) : Android = android[position]
-
-    }
-
-private fun RecyclerView.ViewHolder.bind(item: Android) {
-    TODO("Not yet implemented")
-}
+    private fun getItem(position: Int): Android = android[position]
 
 
-    class ViewHolder(itemView: View,
+    class ViewHolder(
+        itemView: View,
+        listener: (position: Int) -> Unit
     ) : RecyclerView.ViewHolder(itemView) {
         private val image: ImageView = itemView.findViewById(R.id.image)
         private val title: TextView = itemView.findViewById(R.id.title)
+
+        init {
+            itemView.setOnClickListener {
+                val position = adapterPosition
+                if (position != NO_POSITION) {
+                    listener(position)
+                }
+            }
+        }
 
         fun bind(version: Android) {
             image.setImageResource(version.imageAndroid)
@@ -46,3 +52,6 @@ private fun RecyclerView.ViewHolder.bind(item: Android) {
 
 
     }
+
+}
+
